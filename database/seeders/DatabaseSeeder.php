9a54c97c65245ad1156e5315872c80008ab6fee9
Malith Batugedara <mba@elsteel.com>
2025-04-24 16:40:08 +0530

    npm and seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PostSeeder::class,
         ]);
+        $this->call([
+            CommentSeeder::class,
+        ]);
     }
 }
